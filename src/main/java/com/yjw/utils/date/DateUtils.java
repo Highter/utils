e4a0094d6a08c487ac7b0781dfdfd56c8f2c1084
Higher <yjwsmileatlife@126.com>
2016-02-29 21:27:26 +0800

    添加方法：Date类型转换成String类型

---
+++ src/main/java/com/yjw/utils/date/DateUtils.java
@@ -0,0 +1,59 @@
+package com.yjw.utils.date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 时间工具类:001001
+ *@author Orange
+ *@date 下午9:05:40,2016年2月29日
+ *@vesion V1.0
+ *@see yjwsmileatlife@126.com
+ */
+public class DateUtils {
+	
+	/**
+	 * 日期格式：年-月-日  时:分:秒
+	 */
+	public static final String dateFormatYMDHMS = "yyyy-MM-dd HH:mm:ss";
+	
+	/**
+	 * 日期格式：年-月-日  时:分
+	 */
+	public static final String dateFormatYMDHM = "yyyy-MM-dd HH:mm";
+	
+	/**
+	 * 日期格式：年-月-日 
+	 */
+	public static final String dateFormatYMD = "yyyy-MM-dd";
+	
+	/**
+	 * 日期格式：年-月
+	 */
+	public static final String dateFormatYM = "yyyy-MM";
+	
+	/**
+	 * Date类型转换成String类型
+	 * @param date
+	 * @param dateFormate
+	 * @return
+	 */
+	public static String getStringByFormat(Date date,String dateFormat){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+		String dateStr = null;
+		try {
+			dateStr = simpleDateFormat.format(date);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dateStr;
+	}
+	
+	public static void main(String[] args) {
+		Date date = new Date();
+		String dateString = getStringByFormat(date, dateFormatYMDHMS);
+		System.out.println(dateString);
+	}
+	
+}
