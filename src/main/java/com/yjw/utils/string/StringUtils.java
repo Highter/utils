60c4c89b6d1fa212ea9fe42652fdde81382655ad
Higher <yjwsmileatlife@126.com>
2016-03-02 21:39:22 +0800

    1、导入json包 2、添加00020005和00020006功能

--- src/main/java/com/yjw/utils/string/StringUtils.java
+++ src/main/java/com/yjw/utils/string/StringUtils.java
@@ -78,22 +78,69 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils{
 		return i;
 	}
 	
+	/**
+	 * 00020005:替换字符串
+	 * @param str
+	 * @param nowStr
+	 * @param replaceStr
+	 * @return
+	 */
+	public static String replaceString(String str,String nowStr,String replaceStr){
+		nowStr = StringUtils.isBlank(nowStr) ? "" : nowStr;
+		replaceStr = StringUtils.isBlank(replaceStr) ? "" : replaceStr;
+		if(StringUtils.isNotBlank(str)){
+			return str.replaceAll(nowStr, replaceStr);
+		}
+		return "";
+	}
+	
+	 /**
+     * 00020006:判断一个字符串是否为JSONObject,是返回JSONObject,不是返回null
+     * @param args
+     * @return
+     */
+    public static net.sf.json.JSONObject isJSONObject(Object args) {
+        net.sf.json.JSONObject result = null ;
+         
+        if(args instanceof net.sf.json.JSONObject){
+            return (net.sf.json.JSONObject)args;
+        }
+         
+        if(isBlank(args)){
+            return result ;
+        }
+        try {
+            return net.sf.json.JSONObject.fromObject(args);
+        } catch (Exception e) {
+            return result ;
+        }
+    }
+	
 	public static void main(String[] args) {
-		
-		//测试00020001
+		//测试00020001:一次性判断多个或单个对象为空
 		String strNull = "nullStr";
 		String strNotNull = "notNullStr";
 		System.out.println("00020001:"+isBlank(strNull,strNotNull));
 		
-		//测试00020002
+		//测试00020002:一次性判断多个或单个对象不为空
 		System.out.println("00020002:"+isNotBlank(strNull,strNotNull));
 		
-		//测试00020003
-		String str = "Hello world";
-		System.out.println("00020003:"+checkNullToConvert(str));
+		//测试 00020003:处理空对象类型返回字符串类型
+		String str003 = "Hello world";
+		System.out.println("00020003:"+checkNullToConvert(str003));
 		
-		//测试00020004
-		String[] strings = new String[]{"H","e","l","l","o","w","o","r","l","d"};
-		System.out.println("00020004:"+indexOf("l",strings));
+		//测试00020004:判断一个字符串在数组中存在几个
+		String[] strings004 = new String[]{"H","e","l","l","o","w","o","r","l","d"};
+		System.out.println("00020004:"+indexOf("l",strings004));
+		
+		//测试00020005:替换字符串
+		String str0005 = "Hello world";
+		String nowStr005 = "l";
+		String replaceStr005 = "i";
+		System.out.println("00020005:"+replaceString(str0005, nowStr005, replaceStr005));
+		
+		//测试00020006:判断一个字符串是否为JSONObject,是返回JSONObject,不是返回null
+		String arg0006 = "{'userName': 'yjw','password': '123456','email': 'yjwsmileatlife@126.com'}";
+		System.out.println("00020006:"+isJSONObject(arg0006));
 	}
 }
